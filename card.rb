c2ae3f497b647118485c376f83151b33f1b5d69f
Patrick Ewing <patrick.henry.ewing@gmail.com>
2013-03-30 09:58:44 -0700

    Shewing how th' Dogs see their cards

--- card.rb
+++ card.rb
@@ -13,12 +13,24 @@ class Card
 
   def value
     case rank
-      when 'Ace'  then 14
+      when 'Ace' then 14
       when 'King' then 13
       when 'Queen' then 12
       when 'Jack' then 11
-      else; rank
+      else; rank.to_i
     end
+  end
+
+  def face_card?
+    value > 10 && value < 14
+  end
+
+  def ace?
+    rank == 'Ace'
+  end
+
+  def red?
+    [:diamonds, :hearts].include? suit.downcase.to_sym
   end
 
   def to_s
--- game.rb
+++ game.rb
@@ -1,13 +1,28 @@
 require 'deck'
 require 'card'
-require 'player'
+require 'dog'
+require 'rubygems'
+require 'chance'
 
 deck = Deck.new
 
-%w{A 2 3 4 5 6 7 8 9 10 J Q K}.each do |rank|
+%w{2 3 4 5 6 7 8 9 10 Jack Queen King Ace}.each do |rank|
   %w{Spades Hearts Diamonds Clubs}.each_with_index do |suit, i|
     deck.cards << Card.new(rank, suit)
   end
 end
 
-puts deck.cards
+hand1 = 5.times.collect { deck.cards.random_pop}
+
+puts "The true hand:"
+puts hand1
+puts
+puts
+
+Dog.dogs.each do |class_of_dog|
+  dog = class_of_dog.new
+  puts dog.name + " will now read the cards."
+  dog.read hand1
+  puts
+  puts
+end
